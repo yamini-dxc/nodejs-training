702ae36bec58745d47394bb321b7c04c2c5c013e
yamini-dxc <123363831+yamini-dxc@users.noreply.github.com>
2023-02-01 18:08:44 +0530

    Add files via upload

--- day4Assignment.js
+++ day4Assignment.js
@@ -1,63 +1,90 @@
-/*
-1. Create a module which will take in a file and create a copy of the file
-*/
+var http = require("http");
+var path = require("path");
+var url = require("url");
+var qs = require("querystring");
 
-const fs = require('fs');
-const path = require('path');
+const port = 3000;
+var postsArr = [];
 
-fs.copyFile('text1.txt', 'text2.txt', (err) => {
-    if (err) {
-        console.log("Failed to create copy of text1.txt")
-    } else {
-        console.log('Successfully created copy of text1.txt');
-    }
-});
-
-/*
-2. Clone a directory and its child files
-cloneDirectory(path to a directory); return me the cloned directory name
-*/
-
-function cloneDirectory(sourceDirectory) {
-    var destDirectory = sourceDirectory + '_Copy';
-    fs.mkdir(path.join(destDirectory), (err) => {
-        if (err) {
-            console.log("Error in creating the dir" + err);
-        } else {
-            console.log("Directory created successfully");            
-            fs.cp(sourceDirectory, destDirectory, { recursive: true }, (err) => {
-                if (err) {
-                    console.log("Error while cloning the directory " + err);
+var server = http.createServer((request, response) => {
+    var urlObject = url.parse(request.url);
+    if (request.url = "/notes") {
+        if (request.method = "DELETE") {
+            var postToBeDeleted = '';
+            request.on("data", (chunks) => {
+                postToBeDeleted += chunks;
+            })
+            request.on("end", () => {
+                var postToBeDeletedObj = JSON.parse(postToBeDeleted);
+                var pos = postsArr.findIndex(item => item.postsName == postToBeDeletedObj.postsName)
+                if (pos >= 0) {
+                    postsArr.splice(pos, 1);
+                    response.end("Post deleted successfull");
+                    return;
                 } else {
-                    console.log("Directory cloned successfully");
-                    return destDirectory;
+                    response.end("Post not found!")
+                    return;
                 }
-            });
+            })
+            request.on("error", (err) => {
+                console.log("Error in the post request to /post", err);
+                response.end("Deletion failed");
+                return;
+            })
         }
-    })
-    // 
-    
-}
-
-var newDirectory = cloneDirectory("./masterFolder");
-console.log(newDirectory);
-
-
-/*
-3. Function to Check for existence of a file
-doesFileExist(pathtothefile); return a boolean value
-*/
-
-function doesFileExist(filePath) {
-    fs.stat(filePath, (err, data) => {
-        if (err) {
-            console.log('File not found!');
-        } else {
-            console.log('File found! ' + data.isFile() );
-            return data.isFile();
+        if (request.method = "GET" && urlObject.pathName == "/post") {
+            var qsObject = qs.parse(urlObject.query);
+            for (item of Object.keys(qsObject)) {
+                console.log(`${item} : ${qsObject[item]}`);
+            }
+            var pos = postsArr.findIndex(item => item.postsName == qsObject.postsName);
+            if (pos >= 0) {
+                response.end(JSON.stringify(postsArr[pos]));
+            } else {
+                response.statusCode = 402;
+                response.end("data not found");
+            }
+            return;
         }
-    });
-}
+        if (request.method = "PATCH") {
+            var postToBeUpdated = "";
+            request.on("data", (chunks) => {
+                postToBeUpdated += chunks;
+            })
+            request.on("end", (request, response) => {
+                var postToBeUpdatedObj = JSON.stringify(postToBeUpdated);
+                postsArr.push(postToBeUpdatedObj);
+            })
+            request.on("error", (err) => { })
+        }
+        if (request.method = "GET") {
+            //select query
+            response.end(JSON.stringify(postsArr));
+            return;
+        }
+        if (request.method = "POST") {
+            // insert new record
+            //data is coming in the body section
+            var newPosts = "";
+            request.on("data", (chunks) => {
+                newPosts += chunks;
+            })
+            request.on("end", () => {
+                var newPostsObj = JSON.stringify(newPosts);
+                postsArr.push(newPostsObj);
+                response.end("New post insterted");
+            })
+            request.on("error", (err) => {
+                //insertion failed
+                console.log("Error in the post request to /post", err);
+                response.end("Insertion failed");
+                return;
+            })
+        }
+    }
+    response.end("Response from the server for undeveloped url");
+})
 
-var fileExist = doesFileExist('./text1.txt');
-console.log(fileExist);
+server.listen(port, () => {
+    console.log(`Server started at port ${port}`);
+})
